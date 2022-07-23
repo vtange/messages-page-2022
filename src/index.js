4fe56d2663b0440a1ac6ce15aca266b5fc3a9a78
Victor Tang <vtange@users.noreply.github.com>
2022-07-22 18:52:43 -0700

    nplb mascot buttons with countdown on hover, disabled initially

--- src/index.js
+++ src/index.js
@@ -116,3 +116,45 @@ sky.addEventListener("click", function(e){
     lastClick = Date.now();
     generateFirework(e);
 });
+
+// countdown stuff
+var debutN = 1628780400000+31557600000;
+var debutP = 1629039600000+31557600000;
+var debutL = 1628694000000+31557600000;
+var debutB = 1628866800000+31557600000;
+var debuts = [debutN,debutP,debutL,debutB];
+function secondsToHms(d) {
+    d = Number(d);
+    if(d<0) return "00:00:00 🎉";
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return (
+      (h > 0 ? (h>9?h:"0"+h) + ":" : "00:") + (m>9?m:"0"+m) + ":" + (s < 10 ? "0" : "") + s
+    );
+}
+function runTimer(){
+    var time = new Date().getTime();
+    nplb.forEach(function(el,i){
+        var timeleft = (debuts[i] - time)/1000;
+        if (el.getAttribute("done") === "true") return;
+        el.innerHTML = secondsToHms(timeleft);
+        if(timeleft < 0) {
+            el.setAttribute("done","true");
+            //previousElementSibling MUST be <button>
+            el.previousElementSibling.disabled = false;
+        }
+    });
+}
+function get(a){
+    return document.getElementById(a);
+}
+var ne = get("ne");
+var po = get("po");
+var ra = get("ra");
+var bo = get("bo");
+var nplb = [ne,po,ra,bo];
+runTimer();
+window.setInterval(function(){
+    runTimer();
+},1000);
